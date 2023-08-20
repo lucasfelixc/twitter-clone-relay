8fd7dc4158d0b4320ee36d8ff34600d94d2d3587
lucasfelixc <lucasfelixdev@gmail.com>
2023-08-20 09:52:42 -0300

    chore: first structure

--- typegraphql-twitter-api/src/config/auth.ts
+++
@@ -1,6 +0,0 @@
-export default {
-  jwt: {
-    secret: '77bd2a7bf5f53fe81864fd4135cb6cf7',
-    expiresIn: '7d',
-  }
-}
--- typegraphql-twitter-api/src/database/index.ts
+++
@@ -1,6 +0,0 @@
-import mongoose from 'mongoose';
-
-mongoose.connect('mongodb://localhost:27017/twitter', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
